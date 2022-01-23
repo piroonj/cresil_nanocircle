1572ce019df6a2c4f003720d6f3e45d884542de1
piroonj <piroonj@gmail.com>
2022-01-24 06:12:42 +0700

    Upload Utils.py

---
+++ NanoCircle/Utils.py
@@ -0,0 +1,91 @@
+import argparse
+import edlib as ed
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import pybedtools as bt
+from Bio import SeqIO
+import re
+import os
+import pysam as ps
+import matplotlib.pyplot as plt
+import pylab as plt2
+import time
+from collections import defaultdict
+from matplotlib.patches import Rectangle
+from collections import Counter
+import pybedtools
+import math
+
+
+def SamBamParser(bamfile):
+    """ Loads the sam / bam file """
+    # add .fetch(reg,start,end,multiple_iterators=True)
+    file = ps.AlignmentFile(bamfile, "rb")
+    return file
+
+def CIGAR_len(cigar_str):
+    """Returns the actual length of the sequence based on the CIGAR int values"""
+    count = 0
+    for i in re.findall(r'(\d+)([A-Z]{1})', cigar_str):
+        if i[-1] == "M" or i[-1] == "I" or i[-1] == "H":
+            count += int(i[0])
+        elif i[-1] == "D":
+            count -= int(i[0])
+    return count
+
+def CIGAR_pos(cig_str,char_str):
+    """Returns the position in the Cigar string for the given character string"""
+    return [pos for pos, char in enumerate(cig_str) if char == char_str]
+
+def IS_SA(read,mapQ):
+    """returns true if the read in the bamfile is soft-clipped"""
+    if read.cigar[0][0] == 4:
+        if read.has_tag("SA"):
+            if read.mapping_quality >= mapQ:
+                return True
+
+def IS_supp(read,mapQ):
+    """returns true if the read in the bamfile is supplementary"""
+    if read.is_supplementary == True:
+        if read.mapping_quality >= mapQ:
+            return True
+
+def Right_dir(prim_pos,supp_pos):
+    """Checks if the sequencing direction is from left to right"""
+    if prim_pos[0] < supp_pos[0] and prim_pos[-1] < supp_pos[-1]:
+        return True
+
+def Left_dir(prim_pos,supp_pos):
+    """"Checks if the sequencing direction is from right to left"""
+    if prim_pos[0] > supp_pos[0] and prim_pos[-1] > supp_pos[-1]:
+        return True
+
+def Circ_once(prim_pos,supp_pos):
+    """Checks if the sequencing direction is from left to right
+    spanning entire circle at least once"""
+    if prim_pos[0] <= supp_pos[0] and prim_pos[-1] >= supp_pos[-1]:
+        return True
+
+def Right_circ_dir(prim_pos,supp_pos):
+    """Checks if the sequencing direction is from left to right
+    spanning entire circle at least once"""
+    if prim_pos[0] < supp_pos[0] and prim_pos[-1] >= supp_pos[-1]:
+        return True
+
+def Left_circ_dir(prim_pos,supp_pos):
+    """Checks if the sequencing direction is from right to left
+    spanning entire circle at least once"""
+    if prim_pos[0] <= supp_pos[0] and prim_pos[-1] > supp_pos[-1]:
+        return True
+
+def df_float_to_int(df):
+    "Ensures the coordinate values in the dataframe are integers"
+    row_no = df.shape[0]
+    for i in df.columns:
+        for j in range(row_no):
+            if isinstance(df[str(i)][j], str):
+                pass
+            elif isinstance(df[str(i)][j], float):
+                df.at[j, str(i)] = int(df[str(i)][j])
+    return df
